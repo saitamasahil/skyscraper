c6b91e333b844ae06ff3a6191f86c5e6a9ae3387
Gemba <uid0@sdf-eu.org>
2023-12-01 14:56:26 +0100

    Fixes #4: leftover backtick removed and some cleanup

--- update_skyscraper.sh
+++ update_skyscraper.sh
@@ -1,40 +1,52 @@
-#!/bin/bash
+#! /usr/bin/env bash
 {
-    LATEST=$(wget -q -O - "https://api.github.com/repos/Gemba/skyscraper/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`)
+	LATEST=$(wget -q -O - "https://api.github.com/repos/Gemba/skyscraper/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
-    if [ ! -f VERSION ]
-    then
-	echo "VERSION=0.0.0" > VERSION
-    fi
-    source VERSION
+	if [ ! -f VERSION ]; then
+		echo "VERSION=0.0.0" >VERSION
+	fi
+	source VERSION
 
-    handle_error () {
-	local EXITCODE=$?
-	local ACTION=$1
-	rm --force VERSION
-	echo "--- Failed to $ACTION Skyscraper v.${LATEST}, exiting with code $EXITCODE ---"
-	exit $EXITCODE
-    }
+	handle_error() {
+		local EXITCODE=$?
+		local ACTION=$1
+		rm --force VERSION
+		echo "--- Failed to $ACTION Skyscraper v${LATEST}, exiting with code $EXITCODE ---"
+		exit $EXITCODE
+	}
 
-    if [ "$LATEST" != "$VERSION" ]
-    then
-	echo "--- Fetching Skyscraper v.$LATEST ---"
-	wget -N https://github.com/Gemba/skyscraper/archive/"${LATEST}".tar.gz || handle_error "fetch"
-	echo "--- Unpacking ---"
-	tar xvzf "${LATEST}".tar.gz --strip-components 1 --overwrite || handle_error "unpack"
-	rm "${LATEST}".tar.gz
-	echo "--- Cleaning out old build if one exists ---"
-	make --ignore-errors clean
-	rm --force .qmake.stash
-	qmake || handle_error "clean old"
-	echo "--- Building Skyscraper v.$LATEST ---"
-	make -j$(nproc) || handle_error "build"
-	echo "--- Installing Skyscraper v.$LATEST ---"
-	sudo make install || handle_error "install"
-	echo "--- Skyscraper has been updated to v.$LATEST ---"
-    else
-	echo "--- Skyscraper is already the latest version, exiting ---"
-	echo "You can force a reinstall by removing the VERSION file by running rm VERSION. Then rerun ./update_skyscraper.sh afterwards."
-    fi
-    exit
+	if [ "$LATEST" != "$VERSION" ]; then
+		echo
+		echo "--- Fetching Skyscraper v$LATEST ---"
+		tarball="${LATEST}.tar.gz"
+		wget -nv https://github.com/Gemba/skyscraper/archive/"$tarball" || handle_error "fetch"
+
+		echo
+		echo "--- Unpacking ---"
+		tar xzf "$tarball" --strip-components 1 --overwrite || handle_error "unpack"
+		rm -f "$tarball"
+
+		echo
+		echo "--- Cleaning out old build if one exists ---"
+		make --ignore-errors clean
+		rm --force .qmake.stash
+		qmake || handle_error "clean old"
+
+		echo
+		echo "--- Building Skyscraper v$LATEST ---"
+		make -j$(nproc) || handle_error "build"
+
+		echo
+		echo "--- Installing Skyscraper v$LATEST ---"
+		sudo make install || handle_error "install"
+
+		echo
+		echo "--- Skyscraper has been updated to v$LATEST ---"
+	else
+		echo
+		echo "--- Skyscraper is already the latest version, exiting ---"
+		echo "Hint: You can force a reinstall by removing the VERSION file by" 
+		echo "running 'rm VERSION'. Then run ./update_skyscraper.sh again."
+	fi
+	exit
 }
